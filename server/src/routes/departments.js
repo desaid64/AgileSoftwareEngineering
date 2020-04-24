d8c491edcd8fdfaecb016ddf784c269616569845
anchet19 <anchet19@students.rowan.edu>
2020-04-24 15:11:36 -0400

    retrieve all or one department

--- server/src/routes/departments.js
+++ server/src/routes/departments.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import authenticate from '../middlewares/authenticate.js';
 const router = express.Router();
 const db = require("../config/db.js");
 
-router.get('/', (req, res) => {
-  var sql = 'select DepartmentID, DepartmentName from Departments order by DepartmentName';
+router.get('/', authenticate, (req, res) => {
+  const sql = req.isAdmin ?
+    'select DepartmentID, DepartmentName from Departments order by DepartmentName'
+    :
+    `select DepartmentName from Departments where DepartmentID=${req.deptId}`
   db.query(sql, (err, results) => {
     if (err) throw err;
     res.json(results);
