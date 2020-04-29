b1ed32465cb273bccc9fe462a66558ffff56c0f5
anchet19 <anchet19@students.rowan.edu>
2020-04-29 17:09:10 -0400

    changed db connection to use a pool

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,19 @@
 var mysql = require('mysql');
 const config = require('./hidden/config.js');
 
-var connection = mysql.createConnection({
-    host: config.db.host,
-    user: config.db.user,
-    password:config.db.password,
-    database: "HotelManagement_Team_2",
-    insecureAuth: true,
-    multipleStatements:true
+var connection = mysql.createPool({
+  connectionLimit: 100,
+  host: config.db.host,
+  user: config.db.user,
+  password: config.db.password,
+  database: "HotelManagement_Team_2",
+  insecureAuth: true,
+  multipleStatements: true
 })
-connection.connect(function (err){
-    if (err) throw err;
-    console.log("Connected to database!");
-});
+// pool.getConnection(function (err) {
+//   if (err) throw err;
+//   console.log("Connected to database!");
+// });
 
 
 module.exports = connection;
